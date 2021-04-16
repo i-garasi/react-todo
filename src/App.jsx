fd0cff4c918539c10eabc3c0ef43a806632eb67b
i-garasi <44829724+i-garasi@users.noreply.github.com>
2021-04-16 16:04:45 +0900

    update: 全体図のマークアップ

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+
+      <div>
+        <p>未完了TODO</p>
+        <ul>
+          <div>
+            <li>test</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+
+      <div>
+        <p>完了TODO</p>
+        <ul>
+          <div>
+            <li>test</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
